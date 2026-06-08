4ab7651774c65f6834f3fb073b3a0eb0b4fa68f2
Jonathan Lipworth <jonathan.lipworth@gmail.com>
2026-06-08 19:51:27 +0400

    Task 0-1: PlexKit package + identity/headers/PlexRequest

---
+++ PlexKit/Sources/PlexKit/ClientIdentity.swift
@@ -0,0 +1,12 @@
+public struct ClientIdentity: Sendable, Equatable {
+    public let clientIdentifier: String
+    public let product: String
+    public let version: String
+    public let deviceName: String
+    public init(clientIdentifier: String, product: String, version: String, deviceName: String) {
+        self.clientIdentifier = clientIdentifier
+        self.product = product
+        self.version = version
+        self.deviceName = deviceName
+    }
+}
---
+++ PlexKit/Tests/PlexKitTests/SanityTests.swift
@@ -0,0 +1,6 @@
+import Testing
+@testable import PlexKit
+
+@Test func versionExists() {
+    #expect(PlexKit.version == "0.1.0")
+}
